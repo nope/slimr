0170e3a59e73a6b6e0fd16df63eaa7ce3302fc40
Thomas Steiner <tsteinr@gmail.com>
2014-04-04 12:44:28 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+// Config
+define('DB_HOST', 'localhost');
+define('DB_NAME', '');
+define('DB_USER', 'root');
+define('DB_PASSWD', '');
+
+// Autoload vendors
+require_once('vendor/autoload.php');
+
+// Prepare app
+$app = new \Slim\Slim(array(
+	'templates.path' => 'templates'
+));
+
+// Create monolog logger and store logger in container as singleton
+// (Singleton resources retrieve the same log resource definition each time)
+$app->container->singleton('log', function() {
+	$log = new \Monolog\Logger('slimr');
+	$log->pushHandler(new \Monolog\Handler\StreamHandler('logs/app.log', \Monolog\Logger::DEBUG));
+	return $log;
+});
+
+// Prepare view
+$app->view(new \Slim\Views\Twig());
+$app->view->parserOptions = array(
+	'charset' => 'utf-8',
+	'cache' => realpath('templates/cache'),
+	'auto_reload' => true,
+	'strict_variables' => false,
+	'autoescape' => true
+);
+$app->view->parserExtensions = array(new \Slim\Views\TwigExtension());
+
+// Define routes
+foreach (glob('routes/*.php') as $file) {
+	require_once($file);
+}
+
+// Run app
+$app->run();
---
+++ templates/cache/61/c9/614a55b804cea9af03fbe8697899772c8df529bb613ea2c4e3bee365ffe7.php
@@ -0,0 +1,31 @@
+<?php
+
+/* index.twig */
+class __TwigTemplate_61c9614a55b804cea9af03fbe8697899772c8df529bb613ea2c4e3bee365ffe7 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello World";
+    }
+
+    public function getTemplateName()
+    {
+        return "index.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
